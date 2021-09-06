1d5ea7b8c64f27c0bb4a7e0afd293caa55edc548
Maharshi <ihsraham27@gmail.com>
2021-09-06 20:44:40 +0530

    added files for various components. Created snail surface.

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ pygame.init()
 # creating a display surface
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
-
 clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Red')
+sky_surface = pygame.image.load('graphics/sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, (0, 0, 0))
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -20,7 +24,13 @@ while True:
             pygame.quit()
             exit()
 
-    screen.blit(test_surface, (0, 0))
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface, (snail_x_pos, 250))
 
     # draw all our elements
     # update everything
