c7a277704002933d4686b7bd9a7a3ba8dd802cc0
Maharshi <ihsraham27@gmail.com>
2021-09-07 03:09:17 +0530

    Created two game states - game active and game over. Game can be restarted by pressing space key.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+game_active = True
 
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
@@ -21,7 +22,6 @@ snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
 snail_rect = snail_surf.get_rect(midbottom=(snail_x_pos, 300))
 
-
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80, 300))
 player_gravity = 0
@@ -31,31 +31,41 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= 300:
-            if player_rect.collidepoint(event.pos):
-                player_gravity = -20
-        if event.type == pygame.KEYDOWN and player_rect.bottom >= 300:
-            if event.key == pygame.K_SPACE:
-                player_gravity = -20
 
-    screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 300))
-    pygame.draw.rect(screen, '#c0e8ec', score_rect)
-    pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        if game_active:
+            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= 300:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN and player_rect.bottom >= 300:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
+        else:
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                game_active = True
+                snail_rect.left = 800
+    if game_active:
+        screen.blit(sky_surface, (0, 0))
+        screen.blit(ground_surface, (0, 300))
+        pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
 
-    screen.blit(score_surf, score_rect)
-    snail_rect.x -= 4
-    if snail_rect.x < -100:
-        snail_rect.x = 800
-    screen.blit(snail_surf, snail_rect)
+        screen.blit(score_surf, score_rect)
+        snail_rect.x -= 4
+        if snail_rect.x < -100:
+            snail_rect.x = 800
+        screen.blit(snail_surf, snail_rect)
 
-    # Player
-    player_gravity += 1
-    player_rect.y += player_gravity
-    if player_rect.bottom >= 300:
-        player_rect.bottom = 300
-    screen.blit(player_surf, player_rect)
+        # Player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 300:
+            player_rect.bottom = 300
+        screen.blit(player_surf, player_rect)
 
+        # collision
+        if snail_rect.colliderect(player_rect):
+            game_active = False
+    #else:
 
 
     # draw all our elements
