cd9510d325a6d71c95c59d6ff8d2f93360e333ff
Maharshi <ihsraham27@gmail.com>
2021-11-10 14:53:52 +0530

    Made some more changes

--- main.py
+++ main.py
@@ -2,6 +2,14 @@
 import pygame
 from sys import exit
 
+
+def display_score():
+    current_time = (pygame.time.get_ticks() - start_time) // 1000
+    score_surf = test_font.render(f'Score: {current_time}', False, (64, 64, 64))
+    score_rect = score_surf.get_rect(center=(400, 50))
+    screen.blit(score_surf, score_rect)
+
+
 # initializing pygame
 pygame.init()
 
@@ -10,13 +18,14 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
-game_active = True
+game_active = False
+start_time = 0
 
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 
-score_surf = test_font.render('My game', False, (64, 64, 64))
-score_rect = score_surf.get_rect(center=(400, 50))
+title_surf = test_font.render('Runner', False, (64, 64, 64))
+title_rect = title_surf.get_rect(center=(400, 50))
 
 snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_x_pos = 600
@@ -25,6 +34,11 @@ snail_rect = snail_surf.get_rect(midbottom=(snail_x_pos, 300))
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom=(80, 300))
 player_gravity = 0
+
+# Intro screen
+player_stand = pygame.image.load('graphics/player/player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center=(400, 200))
 
 while True:
     for event in pygame.event.get():
@@ -43,14 +57,16 @@ while True:
             if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
                 game_active = True
                 snail_rect.left = 800
+                start_time = pygame.time.get_ticks()
     if game_active:
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        pygame.draw.rect(screen, '#c0e8ec', score_rect)
-        pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect)
+        # pygame.draw.rect(screen, '#c0e8ec', score_rect, 10)
+        # screen.blit(score_surf, score_rect)
+        display_score()
 
-        screen.blit(score_surf, score_rect)
-        snail_rect.x -= 4
+        snail_rect.x -= 5
         if snail_rect.x < -100:
             snail_rect.x = 800
         screen.blit(snail_surf, snail_rect)
@@ -65,8 +81,10 @@ while True:
         # collision
         if snail_rect.colliderect(player_rect):
             game_active = False
-    #else:
-
+    else:
+        screen.fill((94, 129, 162))
+        screen.blit(player_stand, player_stand_rect)
+        screen.blit()
 
     # draw all our elements
     # update everything
